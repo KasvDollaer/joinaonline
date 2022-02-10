8c66960488523564922a14714c8a84bda35963a6
Tinashe <tinasherufu99@gmail.com>
2022-02-10 11:35:06 +0200

    Revert "Revert "Merge branch 'main' into tinashe""
    This reverts commit 317f2b51992a47e77026f164635619e3a1f9e177.

---
+++ shop/ecommerce/tasks.py
@@ -0,0 +1,13 @@
+# from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+
+# @task
+def order_mail(order_id):
+	order = Order.objects.get(id=order_id)
+	subject = 'Joina Online market: Order number {}'.format(order_id)
+	message = 'Good day, {}! You have successfully ordered from Joina Online market.\nYour Order number is {}'.format(order.first_name, order.id)
+	mailer = send_mail(subject, message, 'admin@oloja.com', ['order.email'])
+	return mailer
+
